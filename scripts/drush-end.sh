39a0978d73a3b7357c76a7adb638d24fa0cc969f
Jean Valverde <jeanv@catalyst.net.nz>
2016-05-25 17:35:17 +1200

    Enhance drush script

--- scripts/drush-end.sh
+++ scripts/drush-end.sh
@@ -1,6 +1,15 @@
 # Stop using Drush with this container.
 # . scripts/drush-end.sh
-PS1="${TMP_PS1}"
+
+if [[ "$(basename -- "$0")" == "drush-end.sh" ]]; then
+  echo "Don't run $0, source it." >&2
+  exit 1
+fi
+
+PS1="${DK_TMP_PS1}"
 unset DK_USER
-unset CONTAINER
+unset DK_CONTAINER
+unset DK_DRUPAL_ROOT
+unset DK_TMP_PS1
 unalias drush
+echo "Drush alias restored, bye!"
--- scripts/drush-start.sh
+++ scripts/drush-start.sh
@@ -1,7 +1,61 @@
 # Run with source with arguments to use Drush with this container.
-# . scripts/drush-start.sh apache apache drupaldockercompose_apache_1
-export DK_USER=$1
-export CONTAINER=$2
-export TMP_PS1=$PS1
-alias drush="docker exec -u $DK_USER -it $CONTAINER drush --root=/www/drupal"
-PS1="$PS1 \[\e[1;31m\][$CONTAINER] \[\e[m\]"
+# . scripts/drush-start.sh apache:apache drupaldockercompose_apache_1
+
+if [[ "$(basename -- "$0")" == "drush-start.sh" ]]; then
+  echo "Don't run $0, source it." >&2
+  exit 1
+fi
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ] ; then
+cat <<-HELP
+Drupal drush in container script, create alias so every drush cmd 
+will be executed on the Docker container.
+ Arguments:
+  first argument               Container user and group as apache:apache
+  second argument              Container name from docker-compose ps
+  third argument (optional)    Drupal folder on container, default /www/drupal
+ Options:
+  -h,  --help         Display this help and exit
+
+Usage: . drush-start.sh apache:apache dockercomposedrupal_apache_1
+
+Source . drush-end.sh to stop this Drush session.
+
+HELP
+else
+
+  if [ -z "$1" ]; then
+    echo "[i] Set default user 'apache:apache', set as first argument to override."
+    user="apache:apache"
+  else
+    user=$1
+  fi
+
+  if [ -z "$2" ]; then
+    echo "[i] Set default container name 'dockercomposedrupal_apache_1', set as second argument to override."
+    container='dockercomposedrupal_apache_1'
+  else
+    container=$2
+  fi
+
+  if [[ !$3 ]]; then
+    drupal_root='/www/drupal'
+  else
+    drupal_root=$3
+  fi
+
+  # Check if this container exist.
+  RUNNING=$(docker inspect --format="{{ .State.Running }}" $container 2> /dev/null)
+  if [ $? -eq 1 ]; then
+    echo "[error] $container does not exist."
+  else
+    export DK_USER=$user
+    export DK_CONTAINER=$container
+    export DK_DRUPAL_ROOT=$drupal_root
+    export DK_TMP_PS1=$PS1
+
+    alias drush="docker exec -u $DK_USER -it $DK_CONTAINER drush --root=$DK_DRUPAL_ROOT"
+    PS1="$PS1\[\e[1;31m\][$DK_CONTAINER]> \[\e[m\]"
+  fi
+
+fi
