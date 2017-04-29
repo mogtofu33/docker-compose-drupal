194c7ebbdbf396a50b43c96677cc79d66a8f64c2
Jean Valverde <Mogtofu33@users.noreply.github.com>
2017-04-29 15:49:40 +1200

    Update sample-setup-d8.sh
    Fix composer script, add devel.

--- scripts/sample-setup-d8.sh
+++ scripts/sample-setup-d8.sh
@@ -3,8 +3,6 @@
 # This script is an helper to setup this Drupal 8 with composer on Ubuntu 16.04.
 # This script must be run as root, it should be run after sample-setup.sh in this folder.
 
-SCRIPT_DIR="$(dirname "$0")"
-
 # Variables.
 project_path="/home/ubuntu/docker-compose-drupal"
 project_container_apache="dockercomposedrupal_apache_1"
@@ -14,9 +12,16 @@ drush_bin="$project_container_root/vendor/bin/drush"
 drush_root="--root=$project_container_root/web"
 drush_options="--db-url=mysql://drupal:drupal@mysql/drupal --account-pass=password"
 
+# As we are root, we need this to use composer.
+export COMPOSER_HOME=/home/ubuntu/.composer
 # Setup Drupal 8 composer project.
 /usr/local/bin/composer create-project drupal-composer/drupal-project:8.x-dev $project_root/drupal8 --stability dev --no-interaction
-/usr/local/bin/composer -d=$project_root/drupal8 require "drupal/devel" "drupal/admin_toolbar"
+/usr/local/bin/composer -d=$project_root/drupal8 require "drupal/devel" "drupal/admin_toolbar" "drupal/devel"
 
 # Set-up Drupal.
 docker exec -t --user apache $project_container_apache $drush_bin $drush_root -y site-install $drush_options
+docker exec -t --user apache $project_container_apache $drush_bin $drush_root -y en admin_toolbar
+
+echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+echo -e "[setup::info] Drupal 8 installed, account: admin, password: password\n"
+echo -e "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n"
