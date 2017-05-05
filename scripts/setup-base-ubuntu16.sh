8487848f3ad58056744db8939d28bcddbe6db391
Jean Valverde <Mogtofu33@users.noreply.github.com>
2017-05-06 10:44:07 +1200

    Create setup-base-ubuntu16.sh

---
+++ scripts/setup-base-ubuntu16.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+# This is an helper to setup this docker compose Drupal stack on Ubuntu 16.04.
+# This script must be run as ubuntu user with sudo privileges. From cloud-init
+# Ubuntu user should be a sudoers without password.
+
+# Variables.
+docker_compose_version="1.12.0"
+
+# Fix permissions.
+sudo chown -R ubuntu:ubuntu /home/ubuntu
+
+# Setup docker-compose.
+if [ ! -f "/usr/local/bin/docker-compose" ]; then
+  echo "[setup::info] 1/5 Set-up Docker compose $docker_compose_version..."
+  sudo wget -O /usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$docker_compose_version/docker-compose-Linux-x86_64"
+  sudo chown ubuntu:ubuntu /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+else
+  echo "[setup::info] 1/5 Docker compose already here"
+fi
+
+# Set-up composer.
+if [ ! -f "/usr/local/bin/composer" ]; then
+  echo "[setup::info] 4/5 Set-up Composer and dependencies..."
+  mkdir -p /home/ubuntu/.composer
+  export COMPOSER_HOME=/home/ubuntu/.composer
+  curl -sS https://getcomposer.org/installer | php -- --filename=composer
+  sudo mv composer /usr/local/bin/composer
+  sudo chmod +x /usr/local/bin/composer
+  /usr/local/bin/composer global require "hirak/prestissimo:^0.3" "drupal/coder"
+  echo "COMPOSER_HOME=/home/ubuntu/.composer" >> /home/ubuntu/.profile
+  echo "PATH=\$PATH:/home/ubuntu/.composer/vendor/bin" >> /home/ubuntu/.profile
+else
+  echo "[setup::info] Composer already here!"
+  ## Install dependencies just in case.
+  /usr/local/bin/composer global require "hirak/prestissimo:^0.3" "drupal/coder"
+fi
+
+# Set-up Code sniffer.
+echo "[setup::info] 5/5 Set-up Code sniffer and final steps..."
+$COMPOSER_HOME/vendor/bin/phpcs --config-set installed_paths $COMPOSER_HOME/vendor/drupal/coder/coder_sniffer
+
+# Add drush alias shortcut.
+cat <<EOT >> /home/ubuntu/.bash_aliases
+# Docker
+alias dk='docker'
+# Docker-compose
+alias dkc='docker-compose'
+# Check Drupal coding standards
+alias drcs="phpcs --standard=Drupal --extensions='php,module,inc,install,test,profile,theme,js,css,info,txt'"
+# Check Drupal best practices
+alias drcsbp="phpcs --standard=DrupalPractice --extensions='php,module,inc,install,test,profile,theme,js,css,info,txt,md'"
+# Fix Drupal coding standards
+alias drcsfix="phpcbf --standard=Drupal --extensions='php,module,inc,install,test,profile,theme,js,css,info,txt'"
+EOT
+
+mkdir -p /home/ubuntu/www
+
+echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+echo -e "[setup::info] Install finished!\n"
+echo -e "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n"
