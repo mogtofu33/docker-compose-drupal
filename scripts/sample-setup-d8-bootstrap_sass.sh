23d5db73a3748cc1987f00f4e02297fd792cbe20
Jean Valverde <Mogtofu33@users.noreply.github.com>
2017-05-10 16:47:33 +1200

    Update sample-setup-d8-bootstrap_sass.sh
    Fix paths

--- scripts/sample-setup-d8-bootstrap_sass.sh
+++ scripts/sample-setup-d8-bootstrap_sass.sh
@@ -20,26 +20,26 @@ bootstrap_version="3.3.7"
 docker_cmd="docker exec -t --user apache $project_container_apache "
 name="bootstrap_sass"
 title="Bootstrap Sass"
-theme="$project_container_web_root/web/theme"
+theme="$project_root/drupal/web/themes"
 
 # Add Bootstrap theme of Drupal 8 with composer.
 echo "[setup::info] Install Bootstrap for Drupal 8..."
-/usr/local/bin/composer -d=$project_root/drupal require "drupal/bootstrap"
+#/usr/local/bin/composer -d=$project_root/drupal require "drupal/bootstrap"
 
 # Create bootstrap subtheme
 echo "[setup::info] Create $title subtheme..."
-$docker_cmd mkdir -p $theme/custom
-$docker_cmd cp -r $theme/contrib/bootstrap/starterkits/sass $theme/custom/$name
-$docker_cmd wget -O $theme/custom/$name/$bootstrap_version.tar.gz https://github.com/twbs/bootstrap-sass/archive/v$bootstrap_version.tar.gz
-$docker_cmd tar -xvzf $theme/custom/$name/$bootstrap_version.tar.gz -C $theme/custom/$name/
-$docker_cmd mv $theme/custom/$name/bootstrap-sass-$bootstrap_version $theme/custom/$name/bootstrap
-$docker_cmd rm -f $theme/custom/$name/$bootstrap_version.tar.gz
-$docker_cmd mv $theme/custom/$name/THEMENAME.starterkit.yml $theme/custom/$name/$name.info.yml
-$docker_cmd mv $theme/custom/$name/THEMENAME.libraries.yml $theme/custom/$name/$name.libraries.yml
-$docker_cmd mv $theme/custom/$name/THEMENAME.theme $theme/custom/$name/$name.theme
-$docker_cmd mv $theme/custom/$name/config/install/THEMENAME.settings.yml $theme/custom/$name/config/install/$name.settings.yml
-$docker_cmd mv $theme/custom/$name/config/schema/THEMENAME.schema.yml $theme/custom/$name/config/schema/$name.schema.yml
-$docker_cmd wget -O $theme/custom/$name/config.rb https://gist.githubusercontent.com/Mogtofu33/99a6a764ce0be20d7faa55c7ed315def/raw/18c33d42f539877bf52df4704ecf293e88e7bdfb/config.rb
+mkdir -p $theme/custom
+cp -r $theme/contrib/bootstrap/starterkits/sass/ $theme/custom/$name
+wget -O $theme/custom/$name/$bootstrap_version.tar.gz https://github.com/twbs/bootstrap-sass/archive/v$bootstrap_version.tar.gz
+tar -xvzf $theme/custom/$name/$bootstrap_version.tar.gz -C $theme/custom/$name/
+mv $theme/custom/$name/bootstrap-sass-$bootstrap_version $theme/custom/$name/bootstrap
+rm -f $theme/custom/$name/$bootstrap_version.tar.gz
+mv $theme/custom/$name/THEMENAME.starterkit.yml $theme/custom/$name/$name.info.yml
+mv $theme/custom/$name/THEMENAME.libraries.yml $theme/custom/$name/$name.libraries.yml
+mv $theme/custom/$name/THEMENAME.theme $theme/custom/$name/$name.theme
+mv $theme/custom/$name/config/install/THEMENAME.settings.yml $theme/custom/$name/config/install/$name.settings.yml
+mv $theme/custom/$name/config/schema/THEMENAME.schema.yml $theme/custom/$name/config/schema/$name.schema.yml
+wget -O $theme/custom/$name/config.rb https://gist.githubusercontent.com/Mogtofu33/99a6a764ce0be20d7faa55c7ed315def/raw/18c33d42f539877bf52df4704ecf293e88e7bdfb/config.rb
 
 # Locally edit files.
 sed -i -e 's/THEMETITLE/$title/g' $project_root/drupal/web/themes/custom/$name/$name.info.yml
