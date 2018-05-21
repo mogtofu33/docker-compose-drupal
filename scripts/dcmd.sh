c0306d3fa8375167fcd1ef57dfb0acfd999a236a
Jean <moimog33@gmail.com>
2018-05-21 16:51:01 +1200

    Fix scripts to have a generic env file

--- scripts/dcmd.sh
+++ scripts/dcmd.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-# Default local variables.
-source .env
+_SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$_SOURCE" ]; do # resolve $_SOURCE until the file is no longer a symlink
+  _DIR="$( cd -P "$( dirname "$_SOURCE" )" && pwd )"
+  _SOURCE="$(readlink "$_SOURCE")"
+  [[ $_SOURCE != /* ]] && _SOURCE="$_DIR/$_SOURCE" # if $_SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+_DIR="$( cd -P "$( dirname "$_SOURCE" )" && pwd )"
 
-docker exec \
-  -t \
+source $_DIR/helpers/common.sh
+
+$_DOCKER exec \
+  -it \
   --user "${PROJECT_CONTAINER_USER}" \
-  --interactive "${PROJECT_CONTAINER_NAME}" \
-  "$@"
+  "${PROJECT_CONTAINER_NAME}" "$@"
