0ec14f7f0562a3d789c5e5d8368c1be3463a40a7
Jean Valverde <moimog33@gmail.com>
2019-01-28 13:24:10 +1300

    Make scripts more generic (cloud or local)

--- openstack/install.sh
+++ openstack/install.sh
@@ -5,6 +5,12 @@
 # We assume that docker and docker-compose is properly installed when using this
 # script (From cloud config files in this folder).
 # This script is used with a cloud config setup from this folder.
+#
+# Depends on:
+#   make
+#   git
+#   docker
+#   docker-compose
 
 set -o nounset
 set -o errexit
@@ -12,6 +18,14 @@ trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
 set -o errtrace
 set -o pipefail
 IFS=$'\n\t'
+
+_SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$_SOURCE" ]; do # resolve $_SOURCE until the file is no longer a symlink
+  _DIR="$( cd -P "$( dirname "$_SOURCE" )" && pwd )"
+  _SOURCE="$(readlink "$_SOURCE")"
+  [[ $_SOURCE != /* ]] && _SOURCE="$_DIR/$_SOURCE" # if $_SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+__HERE_DIR="$( cd -P "$( dirname "$_SOURCE" )" && pwd )"
 
 ###############################################################################
 # Help
@@ -25,11 +39,19 @@ IFS=$'\n\t'
 # Print the program help information.
 _print_help() {
   cat <<HEREDOC
-Install Docker Compose Drupal on a Ubuntu distribution, firstly in Openstack.
+Install Docker Compose Drupal on a Ubuntu distribution, firstly in Openstack but
+can be used on a local Ubuntu 16+.
 
 Options:
  --down                Set the stack down at the end of setup
-  -h --help            Show this screen.
+
+ --no-composer         Do not install composer
+ --no-links            Do not set links fro Drush / Drupal console on the system
+ --no-env              Do not set env / aliases on the system
+ --no-fix-perms        Do not fix permissions on the system
+
+ --local               Flag to indicate a local setup, force all previous
+                       options: --no-composer --no-links --no-env --no-fix-perms
 
 Options with argument:
  -s|--stack            Name of stack file to use from ./samples folder,
@@ -37,13 +59,14 @@ Options with argument:
  -i|--install          Install a Drupal profile from
                        ./scripts/install-drupal.sh, eg: drupal, drupal-min...
  -b|--branch           Branch for this project, default is "master"
- -u|--user             Local user name to use, default "ubuntu"
- -g|--group            Local user group to use, default "ubuntu"
- -p|--path             Local project path, default "$HOME/docker-compose-drupal"
+ -u|--user             Local user name to use, default current user
+ -g|--group            Local user group to use, default current group
+ -p|--path             Local project path, default "./docker-compose-drupal"
 
 Usage:
-  ./install.sh -b apache_mysql_php -i drupal-min
-  ./install.sh -b apache_postgres9_php --down
+  ./install.sh -s apache_mysql_php -i drupal-min
+  ./install.sh -s apache_postgres9_php --down
+  ./install.sh --local -s apache_mysql_php
 
 HEREDOC
 printf "\\n"
@@ -55,12 +78,16 @@ printf "\\n"
 _PRINT_HELP=0
 
 # Initialize additional expected option variables.
-__LOCAL_USER="ubuntu"
-__LOCAL_GROUP="ubuntu"
-__PROJECT_PATH="${HOME}/docker-compose-drupal"
+__LOCAL_USER="$(id -u $USER)"
+__LOCAL_GROUP="$(id -g $USER)"
+__PROJECT_PATH="${__HERE_DIR}/docker-compose-drupal"
 __BASE_STACK="full"
 __PUT_DOWN=0
 __INSTALL_DRUPAL=0
+__SET_PERMISSIONS=1
+__INSTALL_COMPOSER=1
+__SET_LINKS=1
+__SET_ENV=1
 __BRANCH="master"
 
 # _require_argument()
@@ -95,6 +122,24 @@ do
       ;;
     --down)
       __PUT_DOWN=1
+      ;;
+    --no-composer)
+      __INSTALL_COMPOSER=0
+      ;;
+    --no-links)
+      __SET_LINKS=0
+      ;;
+    --no-env)
+      __SET_ENV=0
+      ;;
+    --no-fix-perms)
+      __SET_PERMISSIONS=0
+      ;;
+    --local)
+      __INSTALL_COMPOSER=0
+      __SET_LINKS=0
+      __SET_ENV=0
+      __SET_PERMISSIONS=0
       ;;
     -s|--stack)
       _require_argument "${__option}" "${__maybe_param}"
@@ -141,9 +186,36 @@ done
 # Program Functions
 ###############################################################################
 
+_check_prerequisites() {
+  __error=0
+
+  if ! [ -x "$(command -v git)" ]; then
+    printf "\\n[setup::ERROR] Missing git, please install, eg: apt update && apt install git\\n\\n"
+    __error=1
+  fi
+  if ! [ -x "$(command -v make)" ]; then
+    printf "\\n[setup::ERROR] Missing make, please install, eg: apt update && apt install make\\n\\n"
+    __error=1
+  fi
+
+  if ! [ -x "$(command -v docker)" ]; then
+    printf "\\n[setup::ERROR] Missing docker, please install, see https://docs.docker.com/install/linux/docker-ce/ubuntu\\n\\n"
+    __error=1
+  fi
+
+  if ! [ -x "$(command -v docker-compose)" ]; then
+    printf "\\n[setup::ERROR] Missing docker-compose, please install, see https://docs.docker.com/compose/install\\n\\n"
+    __error=1
+  fi
+
+  if [ ${__error} == 1 ]; then
+    exit 1
+  fi
+}
+
 _ensure_permissions() {
   printf "\\n[setup::info] Ensure permissions.\\n\\n"
-  sudo chown -R ${__LOCAL_USER}:${__LOCAL_GROUP} ${HOME}
+  sudo chown -R ${__LOCAL_USER}:${__LOCAL_GROUP} ${__HERE_DIR}
 }
 
 _ensure_docker() {
@@ -155,7 +227,7 @@ _ensure_docker() {
 _install_stack() {
   # Get a Docker compose stack.
   if ! [ -d "${__PROJECT_PATH}" ]; then
-    printf "\\n[setup::info] Get Docker stack %s\\n\\n" "${__BRANCH}"
+    printf "\\n[setup::info] Get Docker stack %s to %s\\n\\n" "${__BRANCH}" "${__PROJECT_PATH}"
     git clone -b ${__BRANCH} https://gitlab.com/mog33/docker-compose-drupal.git ${__PROJECT_PATH}
     if ! [ -f "${__PROJECT_PATH}/docker-compose.tpl.yml" ]; then
       printf "\\n[setup::ERROR] Failed to download Docker compose Drupal :(\\n\\n"
@@ -167,6 +239,10 @@ _install_stack() {
 
   # Set-up and launch this Docker compose stack.
   printf "\\n\\n[setup::info] Prepare Docker stack...\\n\\n"
+  if ! [ -x "$(command -v make)" ]; then
+      printf "\\n[setup::ERROR] Missing make, please install, eg: apt update && apt install make\\n\\n"
+      exit 1
+  fi
   (cd ${__PROJECT_PATH} && make setup)
 
 }
@@ -229,7 +305,7 @@ _up_stack() {
 }
 
 _down_stack() {
-  printf "\\n\\n[setup::info] Down stack...\\n\\n"
+  printf "\\n\\n[setup::info] Put down the stack...\\n\\n"
   sleep 20s
   docker-compose --file "${STACK_ROOT}/docker-compose.yml" down
 }
@@ -256,7 +332,7 @@ alias csfixdr="${HOME}/.config/composer/vendor/bin/phpcbf --standard=Drupal --ex
 EOT
 }
 
-_links() {
+_bin_links() {
   printf "\\n\\n[setup::info] Set links...\\n\\n"
   # Drush and Drupal links.
   if ! [ -f "/usr/local/bin/drush" ] && ! [ -f "/usr/local/bin/drush" ]; then
@@ -267,15 +343,18 @@ _links() {
     sudo ln -s ${STACK_ROOT}/scripts/drupal /usr/local/bin/drupal
     sudo chmod a+x /usr/local/bin/drupal
   fi
+}
+
+_links() {
   # Convenient links.
-  if ! [ -d "${HOME}/drupal" ]; then
-    ln -s ${STACK_DRUPAL_ROOT} ${HOME}/drupal
+  if ! [ -d "${__HERE_DIR}/drupal" ]; then
+    ln -s ${STACK_DRUPAL_ROOT} ${__HERE_DIR}/drupal
   fi
-  if ! [ -d "${HOME}/dump" ]; then
-    ln -s ${STACK_ROOT}/${HOST_DATABASE_DUMP#'./'} ${HOME}/dump
+  if ! [ -d "${__HERE_DIR}/dump" ]; then
+    ln -s ${STACK_ROOT}/${HOST_DATABASE_DUMP#'./'} ${__HERE_DIR}/dump
   fi
-  if ! [ -d "${HOME}/scripts" ]; then
-    ln -s ${STACK_ROOT}/scripts ${HOME}/scripts
+  if ! [ -d "${__HERE_DIR}/scripts" ]; then
+    ln -s ${STACK_ROOT}/scripts ${__HERE_DIR}/scripts
   fi
 }
 
@@ -288,12 +367,21 @@ _get_tools() {
 }
 
 _install_all() {
-  _ensure_permissions
-  _ensure_docker
+
+  _check_prerequisites
+
+  if ! [ ${__SET_PERMISSIONS} == 0 ]; then
+    _ensure_permissions
+    _ensure_docker
+  fi
+
   _install_stack
   _source_common
   _setup_stack
-  _install_composer
+
+  if ! [ ${__INSTALL_COMPOSER} == 0 ]; then
+    _install_composer
+  fi
 
   if ! [ ${__INSTALL_DRUPAL} == 0 ]; then
     _download_drupal
@@ -305,10 +393,20 @@ _install_all() {
     _setup_drupal
   fi
 
-  _env_tasks
+  if ! [ ${__SET_ENV} == 0 ]; then
+    _env_tasks
+  fi
+
   _links
+  if ! [ ${__SET_LINKS} == 0 ]; then
+    _bin_links
+  fi
+
   _get_tools
-  _ensure_permissions
+
+  if ! [ ${__SET_PERMISSIONS} == 0 ]; then
+    _ensure_permissions
+  fi
 
   if ((__PUT_DOWN))
   then
